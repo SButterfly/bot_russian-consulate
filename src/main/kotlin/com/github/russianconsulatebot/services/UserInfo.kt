d99514b9919e11aa260e73560b29807ed0d046b6
Sergei Ermakov <sergei.ermakov@airwallex.com>
2024-03-05 11:16:53 +0100

    Refactor Passport10Service

--- src/main/kotlin/com/github/russianconsulatebot/services/UserInfo.kt
+++ src/main/kotlin/com/github/russianconsulatebot/services/UserInfo.kt
@@ -1,6 +1,8 @@
 package com.github.russianconsulatebot.services
 
 import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import kotlin.random.Random
 
 data class UserInfo(
     val firstName: String,
@@ -12,16 +14,23 @@ data class UserInfo(
     val title: Title,
 ) {
     companion object {
-        val DUMMY = UserInfo(
-            "Ivan",
-            "Ivanov",
-            null,
-            "+79170123456",
-            "teloh47590@aersm.com", // "example@example.com",
-            LocalDate.parse("2000-01-01"),
-            Title.MR,
-        )
+        fun generateDummyUserInfo(): UserInfo {
+            val suffix = Random.nextInt()
+            return UserInfo(
+                "Ivan",
+                "Ivanov_$suffix",
+                null,
+                "+31123456789",
+                "example${suffix}@example.com",
+                LocalDate.parse("2000-01-01"),
+                Title.MR,
+            )
+        }
     }
+
+    val dateOfBirthStr = birthDate.format(DateTimeFormatter.ofPattern("dd"))
+    val monthOfBirthStr = birthDate.format(DateTimeFormatter.ofPattern("MM"))
+    val yearOfBirthStr= birthDate.format(DateTimeFormatter.ofPattern("yyyy"))
 }
 
 enum class Title {
