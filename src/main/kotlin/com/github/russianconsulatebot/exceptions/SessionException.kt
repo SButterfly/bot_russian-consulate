33c100ff8c0f3e19de877de42f93ae2a85b1a8a2
Sergei Ermakov <sergei.ermakov@airwallex.com>
2024-03-23 19:29:23 +0100

    Add "Too many questions" exception

--- src/main/kotlin/com/github/russianconsulatebot/exceptions/SessionException.kt
+++ src/main/kotlin/com/github/russianconsulatebot/exceptions/SessionException.kt
@@ -3,5 +3,4 @@ package com.github.russianconsulatebot.exceptions
 /**
  * Abstract exception for all communication errors.
  */
-open class SessionException(message: String, cause: Throwable? = null) : Exception(message, cause) {
-}
+open class SessionException(message: String, cause: Throwable? = null) : Exception(message, cause)
