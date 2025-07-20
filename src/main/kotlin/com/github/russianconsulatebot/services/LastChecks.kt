3a91d2d480ff3aa2173196e2ff921379a7c5bb5b
Sergei Ermakov <sergeiermakof+bot_russian-consulate@gmail.com>
2025-07-20 13:24:33 +0200

    Added successful attempts rate

--- src/main/kotlin/com/github/russianconsulatebot/services/LastChecks.kt
+++ src/main/kotlin/com/github/russianconsulatebot/services/LastChecks.kt
@@ -5,11 +5,14 @@ import java.time.Instant
 import java.util.concurrent.ArrayBlockingQueue
 import java.util.concurrent.BlockingQueue
 
-private const val CAPACITY = 50
+private const val CAPACITY = 100
 
 @Service
 class LastChecks {
     private val queue: BlockingQueue<String> = ArrayBlockingQueue(CAPACITY)
+
+    var totalNumberOfAttempts = 0
+    var successfulNumberOfAttempts = 0
 
     fun push(msg: String) {
         while (queue.size >= CAPACITY) {
@@ -20,5 +23,14 @@ class LastChecks {
 
     fun get(): List<String> {
         return queue.toList()
+    }
+
+    fun incSuccess() {
+        successfulNumberOfAttempts++
+        totalNumberOfAttempts++
+    }
+
+    fun incFailure() {
+        totalNumberOfAttempts++
     }
 }
